3d12723cf9ae6e1f35a25755718d262c69e012f2
Denis <denis9diaz@hotmail.com>
2025-06-03 17:52:48 +0200

    modificados pequeños errores formularios login y registro. Añadida url completa en verificar (temporal)

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
 
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+
+dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
